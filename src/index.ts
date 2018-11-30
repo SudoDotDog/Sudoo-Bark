4dc1546e33baa3bc592d3783917cfa10d4547d23
WMXPY <wm@mengw.io>
2018-11-29 22:03:09 -0600

    1.2.1 add map functions

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@
 
 import * as _Array from './array/import';
 import * as _Date from './date/import';
+import * as _Map from './map/import';
 import * as _String from './string/import';
 import * as _Table from './table/import';
 
-export { _Array, _String, _Date, _Table };
+export { _Array, _Date, _Map, _String, _Table };
 
