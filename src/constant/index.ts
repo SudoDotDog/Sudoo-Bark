97ed88dec70dffc86a15c846c7ae64d0847b46ce
WMXPY <wm@mengw.io>
2019-04-07 22:20:45 -0500

    init constant package

---
+++ src/constant/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @author WMXPY
+ * @namespace Constant
+ * @description Index
+ */
+
+import { StringBuffer } from './string';
+
+const exportee = {
+    StringBuffer,
+};
+
+export { exportee as _Constant };
+export { StringBuffer };
+
+export default exportee;
