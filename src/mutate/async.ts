bdfcf568fe22b6fee533a3636a210994770a7519
WMXPY <wm@mengw.io>
2019-04-26 12:38:30 -0500

    add async rebuild, reducer and unit tests

--- src/mutate/async.ts
+++ src/mutate/async.ts
@@ -4,14 +4,42 @@
  * @description Async
  */
 
-export const asyncMap = async <T = any, R = any>(arr: T[], func: (current: T, index: number, arr: T[]) => R): Promise<any> => {
+export const asyncMap = async <T = any, R = any>(arr: T[], func: (current: T, index: number, arr: T[]) => Promise<R>): Promise<R[]> => {
 
-    const response: R[] = [];
     const length: number = arr.length;
+    const response: R[] = new Array(length);
 
     for (let i = 0; i < length; i++) {
         const result: R = await func(arr[i], i, arr);
-        response.push(result);
+        response[i] = result;
+    }
+
+    return response;
+};
+
+export const asyncReduce = async <T = any, R = any>(arr: T[], func: (previous: R, current: T, index: number, arr: T[]) => Promise<R>, initial: R): Promise<R> => {
+
+    const length: number = arr.length;
+    let current: R = initial;
+
+    for (let i = 0; i < length; i++) {
+        current = await func(current, arr[i], i, arr);
+    }
+
+    return current;
+};
+
+export const asyncRebuild = async <T = any, R = any>(arr: T[], func: (current: T, index: number, arr: T[]) => Promise<R | undefined>): Promise<R[]> => {
+
+    const length: number = arr.length;
+    const response: R[] = [];
+
+    for (let i = 0; i < length; i++) {
+        const result: R | undefined = await func(arr[i], i, arr);
+
+        if (result) {
+            response.push(result);
+        }
     }
 
     return response;
---
+++ test/mutate/async.test.ts
@@ -0,0 +1,91 @@
+/**
+ * @author WMXPY
+ * @namespace Mutate
+ * @description Async
+ * @override Unit
+ */
+
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { asyncMap, asyncRebuild, asyncReduce } from '../../src/mutate/async';
+
+describe('Given [Async] helper functions', (): void => {
+
+    const chance: Chance.Chance = new Chance('mutate-async');
+
+    const getSmallRandomNumber = () => chance.natural({ min: 0, max: 100 });
+
+    describe('Given a [AsyncMap] function', () => {
+
+        it('should be able to return mapped', async (): Promise<void> => {
+
+            const num1: number = getSmallRandomNumber();
+            const num2: number = getSmallRandomNumber();
+            const num3: number = getSmallRandomNumber();
+            const from: number[] = [num1, num2, num3];
+
+            const func = async (value: number, index: number, arr: number[]): Promise<number> => {
+                return value + index + arr.length;
+            };
+
+            expect(await asyncMap(from, func)).to.be.deep.equal([num1 + 3, num2 + 4, num3 + 5]);
+        });
+    });
+
+    describe('Given a [AsyncReduce] function', () => {
+
+        it('should be able to return reduced', async (): Promise<void> => {
+
+            const num1: number = getSmallRandomNumber();
+            const num2: number = getSmallRandomNumber();
+            const num3: number = getSmallRandomNumber();
+            const from: number[] = [num1, num2, num3];
+
+            const func = async (previous: number, value: number, index: number, arr: number[]): Promise<number> => {
+                return previous + value + index + arr.length;
+            };
+
+            expect(await asyncReduce(from, func, 0)).to.be.deep.equal(num1 + num2 + num3 + 3 + 9);
+        });
+    });
+
+    describe('Given a [AsyncRebuild] function', () => {
+
+        it('should be able to return rebuilt', async (): Promise<void> => {
+
+            const num1: number = getSmallRandomNumber();
+            const num2: number = getSmallRandomNumber();
+            const num3: number = getSmallRandomNumber();
+            const from: number[] = [num1, num2, num3];
+
+            const func = async (value: number, index: number, arr: number[]): Promise<number | undefined> => {
+                if (index % 2 === 0) {
+                    return value + arr.length;
+                }
+                return;
+            };
+
+            expect(await asyncRebuild(from, func)).to.be.deep.equal([num1 + 3, num3 + 3]);
+        });
+
+        it('should be able to return timeout rebuilt', async (): Promise<void> => {
+
+            const num1: number = getSmallRandomNumber();
+            const num2: number = getSmallRandomNumber();
+            const num3: number = getSmallRandomNumber();
+            const from: number[] = [num1, num2, num3];
+
+            const func = (value: number, index: number, arr: number[]): Promise<number | undefined> =>
+                new Promise<number | undefined>((resolve: (value?: number) => void) => {
+                    setTimeout(() => {
+                        if (index % 2 === 0) {
+                            resolve(value + arr.length);
+                        }
+                        resolve();
+                    }, 1);
+                });
+
+            expect(await asyncRebuild(from, func)).to.be.deep.equal([num1 + 3, num3 + 3]);
+        });
+    });
+});
